3952ccc1b75ff3f57ac22eb41232196d75cd6c42
Andrew Natoli <AndrewNatoli@AndrewNatoli.com>
2016-01-04 19:44:08 -0500

    Can't auto-exec installer after composer install. Womp womp. :/

--- cli-install.php
+++ cli-install.php
@@ -1,4 +1,30 @@
 <?php
+/*
+ * Run this via command line for a guided approach to creating your config.php!
+ *
+ * I was hoping to have this run after creating the project with composer but composer forces you through with
+ * blank inputs. Either way I thought it would be fun to create an installer so here it is. This will ask you
+ * for your Trello API keys then make sure they work before writing config.php. It helps make sure you have
+ * things set up right without having to try pushing to your repo. Enjoy! - Andrew
+ *
+ * ---
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Desktop Solutions Software
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 if(php_sapi_name() != "cli") {
     die("This can only be run via command line.");
 }
@@ -16,13 +42,12 @@ $file = fopen("checkWriteAccess.php", "w") or die("Can not write to this directo
 fclose($file);
 unlink("checkWriteAccess.php");
 
-print "We can try and set up your config.php here if that's cool with you.\n";
+print "We can set up your config.php here if that's cool with you.\n";
 print "Do you have your Trello tokens ready? [Y/n]: ";
 
 if(strtolower(trim(fgets(STDIN))) != "y") {
-    print "\nOkay. You can copy config-example.php to config.php and set up the API tokens when you're ready.";
-    print "\nYou can also run this cli-install.php script again.";
-    sleep(3);
+    print "\nOkay. Come back and run this script again when you're ready.";
+    print "\nYou can also copy config-example.php to config.php and edit the file yourself.";
     exit;
 }
 
